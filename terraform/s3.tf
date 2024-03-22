a2f600fa673d8c8fe6fe37fd4c2db05179ad6340
Chris Marget <5470536+chrismarget@users.noreply.github.com>
2024-03-22 19:46:53 -0400

    remove dead code

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,15 +2,6 @@ resource "aws_s3_bucket" "registry" {
   bucket = "jtaf-registry"
 }
 
-#resource "aws_s3_bucket_public_access_block" "registry" {
-#  bucket = aws_s3_bucket.registry.bucket
-#
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
-
 data "aws_iam_policy_document" "registry_bucket" {
   statement {
     effect     = "Allow"
